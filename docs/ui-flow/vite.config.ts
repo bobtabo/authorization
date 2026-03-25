322ce3038977156bc891a8f0ec5b27b708ba850a
bobtabo <bobtabo.buhibuhi@gmail.com>
2026-03-26 00:37:55 +0900

    FigmaAI作成の画面フローをVercelビルドが通るように改善2

--- docs/ui-flow/vite.config.ts
+++ docs/ui-flow/vite.config.ts
@@ -4,4 +4,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    // SVG を data: URL に埋め込まずファイルとして出す（<img> + data:image/svg+xml は環境によって描画されないことがある）
+    assetsInlineLimit: 0,
+  },
 });
